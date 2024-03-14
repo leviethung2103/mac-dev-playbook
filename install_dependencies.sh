15353ff261bdef6371a09369f2cd86f27ce8d14a
Le Viet Hung (Jack Ryan) <leviethung1280@gmail.com>
2024-03-14 23:11:30 +0700

    Add new packages

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,3 +4,9 @@ echo "Installing nodejs"
 nvm install --lts
 echo "Checking nodejs version"
 node --version
+echo "Downloading zsh auto suggestion"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+echo "Downloading zsh syntax highlighting"
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+
