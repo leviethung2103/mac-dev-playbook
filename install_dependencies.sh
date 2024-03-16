356584861c708fa7f0ed86c6329ea9dd4e607ff8
Le Viet Hung (Jack Ryan) <leviethung1280@gmail.com>
2024-03-16 21:48:31 +0700

    add nvm and lazygit

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,5 +8,7 @@ echo "Downloading zsh auto suggestion"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 echo "Downloading zsh syntax highlighting"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-
-
+echo "Install the NVM"
+brew install nvm 
+echo "Install lazygit"
+brew install lazygit
